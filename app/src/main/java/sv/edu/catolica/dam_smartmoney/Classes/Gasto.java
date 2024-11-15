83ade0ff8c6f399e7b46b31cbb7fbee287eecf26
Gungrave <jorgefranciscocz@gmail.com>
2024-11-14 23:50:52 -0600

    Some fixes

--- app/src/main/java/sv/edu/catolica/dam_smartmoney/Classes/Gasto.java
+++ app/src/main/java/sv/edu/catolica/dam_smartmoney/Classes/Gasto.java
@@ -17,6 +17,7 @@ public class Gasto {
         this.pagoGasto = pagoGasto;
     }
 
+    // Getters and setters for each field
     public int getId() { return id; }
     public String getNombreGasto() { return nombreGasto; }
     public double getCantidad() { return cantidad; }
